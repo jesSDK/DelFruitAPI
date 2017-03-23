77da581c0ca2028ff8ad846a290fec8733e18fc6
kanichiwah <killzone135@gmail.com>
2017-03-23 00:18:55 +0000

    Added API Solution and Testing Soultion

---
+++ DFAPIDebugging/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DelfruitAPI;
+
+namespace DFAPIDebugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string motd = "";
+            string gamename = "";
+            string url = "";
+              
+          //gets message of the day and stores it in supplied string
+            Console.WriteLine(DF.web.Getmotd(motd));
+            Console.WriteLine("");
+            Console.WriteLine(DF.web.roll(url, gamename));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DelfruitAPI/DF.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using HtmlAgilityPack;
+using System.Collections.Specialized;
+
+namespace DelfruitAPI
+{
+    public class DF
+    {
+        public class web
+        {
+            public static String Getmotd(string motd)
+            {
+                Console.WriteLine("Delfruit API - Getting Message of the Day!");
+                HtmlWeb hweb = new HtmlWeb();
+                HtmlDocument doc;
+                string temp = "";
+                try
+                {
+                    doc = hweb.Load("http://www.delicious-fruit.com/");
+                    foreach (HtmlNode div1 in doc.DocumentNode.SelectNodes("//div[@style='position: relative; padding: 0.5em; background: #e8e8e8;']/p"))
+                    {
+                        
+                          //  Console.WriteLine("MOTD is: " + div1.InnerText.ToString());
+                            motd = div1.InnerText.ToString();
+                            temp = div1.InnerText.ToString();
+                         //   Console.ReadKey();
+                        
+                        
+                    }
+                    return temp;
+                }catch(Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    Console.ReadKey();
+                    return temp;
+                }
+                
+            }
+
+
+            public static String roll(string url, string gamename)
+            {
+                Console.WriteLine("Delfruit API - Getting random game!");
+                HtmlWeb hweb = new HtmlWeb();
+                HtmlDocument doc;
+                url = "";
+                gamename = "";
+                try
+                {
+                    doc = hweb.Load("http://www.delicious-fruit.com/ratings/game_details.php?random=1");
+                    hweb.ResponseUri.AbsolutePath.ToString();
+                    url = hweb.ResponseUri.AbsolutePath.ToString();
+                    return url;
+                    foreach (HtmlNode gametitle in doc.DocumentNode.SelectNodes("//h1[@style='word-wrap: break-word;']"))
+                    {
+                        gamename = gametitle.InnerText.ToString();
+                        return gamename;
+                    }
+                }catch(Exception ex)
+                {
+                    return ex.ToString();
+                }
+            }
+        }
+    }
+}
